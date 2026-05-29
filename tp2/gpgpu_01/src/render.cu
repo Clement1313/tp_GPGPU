d55c739313a0581a430180e86ebcc288289ad907
clement.gregoire <clement.gregoire@epita.fr>
2026-05-29 15:49:42 +0200

    tp2- ex3

--- tp2/gpgpu_01/src/render.cu
+++ tp2/gpgpu_01/src/render.cu
@@ -3,6 +3,8 @@
 
 #include "render.hpp"
 
+__device__ uchar4 heat_lut_color(float x);
+
 [[gnu::noinline]]
 void _abortError(const char* msg, const char* fname, int line)
 {
@@ -22,6 +24,110 @@ struct rgba8_t
     std::uint8_t b;
     std::uint8_t a;
 };
+
+///
+/// \param buffer Input buffer of type (uchar4 or uint32_t)
+/// \param width Width of the image
+/// \param height Height of the image
+/// \param pitch Size of a line in bytes
+/// \param max_iter Maximum number of iterations
+__global__ void apply_LUT(char* buffer, int width, int height, size_t pitch, int max_iter, const uchar4* LUT) {
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+    int y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    if (x >= width || y >= height)
+        return;
+
+    int* iter_line = reinterpret_cast<int*>(buffer + y * pitch);
+    uchar4* color_line = reinterpret_cast<uchar4*>(buffer + y * pitch);
+
+    int iteration = iter_line[x];
+    if (iteration < 0)
+        iteration = 0;
+    if (iteration > max_iter)
+        iteration = max_iter;
+
+    color_line[x] = LUT[iteration];
+}
+
+/// This function is single thread for now!
+///
+/// \param buffer Input buffer of type (uchar4 or uint32_t)
+/// \param width Width of the image
+/// \param height Height of the image
+/// \param pitch Size of a line in bytes
+/// \param max_iter Maximum number of iterations
+/// \param LUT Output look-up table
+__global__ void compute_LUT(const char* buffer, int width, int height,
+                            size_t pitch, int max_iter, uchar4* LUT)
+{
+    (void)buffer;
+    (void)width;
+    (void)height;
+    (void)pitch;
+
+    if (blockIdx.x != 0 || blockIdx.y != 0 || blockIdx.z != 0 ||
+        threadIdx.x != 0 || threadIdx.y != 0 || threadIdx.z != 0)
+        return;
+
+    if (max_iter <= 0)
+    {
+        LUT[0] = make_uchar4(0, 0, 0, 255);
+        return;
+    }
+
+    for (int iteration = 0; iteration <= max_iter; ++iteration)
+    {
+        float t = (float)iteration / (float)max_iter;
+        LUT[iteration] = heat_lut_color(t);
+    }
+}
+
+// Compute the number or iteration of the fractal per pixel and store the result
+// in *buffer*.
+/// Note that a 32-bits location can be used to store an integer (int32) or a
+/// color (uchar4).
+///
+/// \param buffer Input buffer of type (uchar4 or uint32_t)
+/// \param width Width of the image
+/// \param height Height of the image
+/// \param pitch Size of a line in bytes
+/// \param max_iter Maximum number of iterations
+__global__ void compute_iter(char* buffer, int width, int height, size_t pitch,
+                             int max_iter)
+{
+    // float denum = width * width + height * height;
+
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+    int y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    if (x >= width || y >= height)
+        return;
+
+    int* lineptr = reinterpret_cast<int*>(buffer + y * pitch);
+
+    float halfW = (float)width * 0.5f;
+    float halfH = (float)height * 0.5f;
+    float nx = ((float)x - halfW) / halfW;
+    float ny = ((float)y - halfH) / halfH;
+    const float scaleX = 1.5f;
+    const float scaleY = scaleX * ((float)height / (float)width);
+    float mx0 = -0.5f + nx * scaleX;
+    float my0 = -ny * scaleY;
+
+    float mx = 0.0f, my = 0.0f;
+    int iteration = 0;
+
+    while ((mx * mx + my * my) < 4.0f && iteration < max_iter)
+    {
+        float mxtemp = mx * mx - my * my + mx0;
+        my = 2.0f * mx * my + my0;
+        mx = mxtemp;
+        ++iteration;
+    }
+
+    lineptr[x] = iteration;
+}
 
 rgba8_t heat_lut(float x)
 {
@@ -125,25 +231,40 @@ void render(char* hostBuffer, int width, int height, std::ptrdiff_t stride,
     // Allocate device memory
     char* devBuffer;
     size_t pitch;
+    uchar4* devLUT = nullptr;
 
     rc = cudaMallocPitch(&devBuffer, &pitch, width * sizeof(rgba8_t), height);
     if (rc)
         abortError("Fail buffer allocation");
 
-    // Run the kernel with blocks of size 64 x 64
+    rc = cudaMalloc(&devLUT, (n_iterations + 1) * sizeof(uchar4));
+    if (rc)
+        abortError("Fail LUT allocation");
     {
         int bsize = 32;
         int w = std::ceil((float)width / bsize);
         int h = std::ceil((float)height / bsize);
 
-        spdlog::debug("running kernel of size ({},{})", w, h);
+        spdlog::debug("running kernels of size ({},{})", w, h);
 
         dim3 dimBlock(bsize, bsize);
         dim3 dimGrid(w, h);
-        mykernel<<<dimGrid, dimBlock>>>(devBuffer, width, height, pitch);
+
+        compute_iter<<<dimGrid, dimBlock>>>(devBuffer, width, height, pitch,
+                                            n_iterations);
+        if (cudaPeekAtLastError())
+            abortError("compute_iter error");
+
+        compute_LUT<<<1, 1>>>(devBuffer, width, height, pitch, n_iterations,
+                              devLUT);
+        if (cudaPeekAtLastError())
+            abortError("compute_LUT error");
+
+        apply_LUT<<<dimGrid, dimBlock>>>(devBuffer, width, height, pitch,
+                                         n_iterations, devLUT);
 
         if (cudaPeekAtLastError())
-            abortError("Computation Error");
+            abortError("apply_LUT error");
     }
 
     // Copy back to main memory
@@ -156,4 +277,8 @@ void render(char* hostBuffer, int width, int height, std::ptrdiff_t stride,
     rc = cudaFree(devBuffer);
     if (rc)
         abortError("Unable to free memory");
+
+    rc = cudaFree(devLUT);
+    if (rc)
+        abortError("Unable to free LUT memory");
 }
